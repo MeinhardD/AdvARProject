e51ca3681b2134149a5831f6d32900b3dca8a02b
Frode S. N. Pedersen <fsnpedersen@gmail.com>
2018-05-03 11:33:52 +0200

    added buttons that toggles stages

--- AuthoringLegoModels/Assets/Code/ViewModel.cs
+++ AuthoringLegoModels/Assets/Code/ViewModel.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ViewModel : MonoBehaviour {
 
+
+
     public void Button_Click()
     {
-        Debug.Log("Hello world");
+        Scene scene = SceneManager.GetActiveScene();
+        Debug.Log("Active scene is: " + scene.name);
+
+        if (scene.name.Equals("Step1"))
+        {
+            SceneManager.LoadScene("Steps/Step2");
+            Debug.Log("Change done! Step2 is a go!");
+        }
+        if (scene.name.Equals("Step2"))
+        {
+            SceneManager.LoadScene("Steps/Step3");
+            Debug.Log("Change done! Step3 is a go!");
+        }
     }
 
 }
