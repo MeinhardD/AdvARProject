1a2c539740a8b00477c8c18e936a4726e04c4f87
Frode S. N. Pedersen <fsnpedersen@gmail.com>
2018-05-03 11:04:50 +0200

    added button

---
+++ AuthoringLegoModels/Assets/Code/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewModel : MonoBehaviour {
+
+    public void Button_Click()
+    {
+        Debug.Log("Hello world");
+    }
+
+}
