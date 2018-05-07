ddc8717e3a8f462a4b24b9d640ec3ee1fce87d74
Unknown <meinhard.dam@gmail.com>
2018-05-07 11:38:36 +0200

    First iteration of animation in script

--- AuthoringLegoModels/Assets/Code/TrackingModels.cs
+++ AuthoringLegoModels/Assets/Code/TrackingModels.cs
@@ -7,10 +7,13 @@ public class TrackingModels : MonoBehaviour {
 
     public ImageTargetBehaviour currentPart;
     public ImageTargetBehaviour nextPart;
+    private Vector3 model1Position;
+    private Vector3 model2Position;
 
     // Use this for initialization
 	void Start () {
-		
+        model1Position = GameObject.Find("Model1").transform.position;
+        model2Position = GameObject.Find("Model2").transform.position;
 	}
 	
 	// Update is called once per frame
@@ -22,7 +25,18 @@ public class TrackingModels : MonoBehaviour {
         if (tracked1)
         {
             if (tracked2)
+            {
                 Debug.Log("Put the part with a sphere on the part with a cube");
+
+                int increment = 1;
+                float epsilon = 0.05f;
+                if (Mathf.Abs(2 - model1Position.z) < epsilon || Mathf.Abs(-2 - model1Position.z) < epsilon)
+                {
+                    increment = increment * -1;
+                }
+
+                model2Position.z = model2Position.z + increment;
+            }               
             else
                 Debug.Log("Can't find the next part");
         }
