f8bdcecbf9ce97c2a9629ee8442fa31e54851700
Unknown <meinhard.dam@gmail.com>
2018-05-17 11:19:28 +0200

    Animation is nice now

--- AuthoringLegoModels/Assets/Code/TrackingModels.cs
+++ AuthoringLegoModels/Assets/Code/TrackingModels.cs
@@ -7,15 +7,21 @@ public class TrackingModels : MonoBehaviour {
 
     public ImageTargetBehaviour currentPart;
     public ImageTargetBehaviour nextPart;
-    private Vector3 model1Position;
-    private Vector3 model2Position;
+    private GameObject model1;
+    private GameObject model2;
+    private GameObject model2Target;
+    private Vector3 model2LocalPos;
 
     // Use this for initialization
 	void Start () {
-        model1Position = GameObject.Find("Model1").transform.position;
-        model2Position = GameObject.Find("Model2").transform.position;
-	}
-	
+        model1 = GameObject.Find("Model 1");
+        model2 = GameObject.Find("Model 2");
+        model2Target = GameObject.Find("Image_Part2");
+        model2LocalPos = model2.transform.localPosition;
+    }
+
+    int toggle = 1;
+    int counter = 0;
 	// Update is called once per frame
 	void Update () {
 
@@ -26,17 +32,29 @@ public class TrackingModels : MonoBehaviour {
         {
             if (tracked2)
             {
-                Debug.Log("Put the part with a sphere on the part with a cube");
+                Vector3 increment = (model1.transform.position - model2.transform.position).normalized / 3;
 
-                int increment = 1;
-                float epsilon = 0.05f;
-                if (Mathf.Abs(2 - model1Position.z) < epsilon || Mathf.Abs(-2 - model1Position.z) < epsilon)
+                float epsilon = 0.1f;
+                if (Mathf.Abs(model1.transform.position.z - model2.transform.position.z) < epsilon
+                    && counter < 50)
                 {
-                    increment = increment * -1;
+                    counter += 1;                   
+                }
+                else if (Mathf.Abs(model1.transform.position.z - model2.transform.position.z) < epsilon
+                    && counter >= 50)
+                {
+                    counter = 0;
+                    model2.transform.localPosition = model2LocalPos;
+                }
+                else
+                {
+                    model2.transform.position += increment;
                 }
 
-                model2Position.z = model2Position.z + increment;
-            }               
+                Debug.Log("The z position of Model 1 is: " + model1.transform.position.z);
+                Debug.Log("The z position of Model 2 is: " + model2.transform.position.z);
+                Debug.Log("Counter: " + counter);
+            }
             else
                 Debug.Log("Can't find the next part");
         }
