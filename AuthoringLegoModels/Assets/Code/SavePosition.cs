9879beea7ec12042cb0e7735af88443d6e3eec2f
Unknown <meinhard.dam@gmail.com>
2018-06-03 21:46:15 +0200

    Generalized the authoring part

--- AuthoringLegoModels/Assets/Code/SavePosition.cs
+++ AuthoringLegoModels/Assets/Code/SavePosition.cs
@@ -4,38 +4,36 @@ using UnityEngine;
 public class SavePosition : MonoBehaviour
 {
 
-    GameObject model1;
-    GameObject model2;
+    public GameObject intermediateMachine;
+    public GameObject component;
 
-    Vector3 model1Pos;
-    Vector3 model2Pos;
+    Vector3 intermediateMachinePos;
+    Vector3 componentPos;
     Vector3 relativeVector;
 
-    Vector3 model1EulerAngles;
-    Vector3 model2EulerAngles;
+    Vector3 intermediateMachineEulerAngles;
+    Vector3 componentEulerAngles;
     Vector3 relativeEulerAngles;
 
     public void Button_Click()
     {
         Debug.Log("Saved position");
-        model1 = GameObject.Find("Image_Part1");
-        model2 = GameObject.Find("Image_Part2");
         string path = Directory.GetCurrentDirectory() + "\\Assets\\Code\\instructions.txt";
 
-        if (model1Pos.Equals(new Vector3(0.0f, 0.0f, 0.0f)))
+        if (intermediateMachinePos.Equals(new Vector3(0.0f, 0.0f, 0.0f)))
         {
             Debug.Log("First click");
-            model1Pos = model1.transform.position;
-            model1EulerAngles = model1.transform.eulerAngles;
+            intermediateMachinePos = intermediateMachine.transform.position;
+            intermediateMachineEulerAngles = intermediateMachine.transform.eulerAngles;
         }
-        else if (!model1Pos.Equals(new Vector3(0.0f, 0.0f, 0.0f)) && model2Pos.Equals(new Vector3(0.0f, 0.0f, 0.0f)))
+        else if (!intermediateMachinePos.Equals(new Vector3(0.0f, 0.0f, 0.0f)) && componentPos.Equals(new Vector3(0.0f, 0.0f, 0.0f)))
         {
             Debug.Log("Second click");
-            model2Pos = model2.transform.position;
-            model2EulerAngles = model2.transform.eulerAngles;
+            componentPos = component.transform.position;
+            componentEulerAngles = component.transform.eulerAngles;
 
-            relativeVector = model2Pos - model1Pos;
-            relativeEulerAngles = model1EulerAngles - model2EulerAngles;
+            relativeVector = componentPos - intermediateMachinePos;
+            relativeEulerAngles = intermediateMachineEulerAngles - componentEulerAngles;
 
             if (!File.Exists(path))
             {
